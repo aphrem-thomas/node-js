5446869770bca2420a885ee0c5252f0b1d6fd075
aphthoma <aphthoma@cisco.com>
2022-12-04 22:00:18 +0530

    server updated for body parser

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import Express from 'express';
 import Path from 'path';
 import todoRouter from './routes/todo';
+import bodyParser from 'body-parser';
 var cors = require('cors');
 // Constants
 const PORT = 8080;
@@ -9,6 +10,8 @@ const HOST = '0.0.0.0';
 
 // App
 const app = Express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(Express.static(Path.join(__dirname, 'frontend', 'build')));
 
